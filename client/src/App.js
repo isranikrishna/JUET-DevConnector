bbb60db76b3ec1587fbf3aa57349fa2afbc590b4
isranikrishna <kreyisrani@gmail.com>
2020-07-18 21:50:43 +0530

    edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,36 @@
-import React , {Fragment , useEffect} from 'react';
-import { BrowserRouter as Router , Route , Switch} from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Dashboard from './components/dashboard/Dashboard'
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
 
-import { Provider} from 'react-redux'
-import store from './store'
-import Alert from './components/layout/Alert'
-import { loadUser} from './actions/auth'
-import setAuthToken from './Utils/setAuthToken'
-import PrivateRoute from './components/routing/PrivateRoute'
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './Utils/setAuthToken';
 
 import './App.css';
 
-
-
-const  App = () => {
-  useEffect( () => {
-    setAuthToken(localStorage.token)
-    store.dispatch(loadUser())
-  } ,[])
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
 
   return (
-
-  <Provider store={store}>
-  <Router>
-   <Fragment>
-     <Navbar/>
-     <Route exact path='/' component={Landing} />
-      <section className="container">
-        <Alert />
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <PrivateRoute exact path='/dashboard' component= {Dashboard} />
-
-        </Switch>
-      </section>
-   </Fragment>
-   </Router>
-   </Provider>
-)
-  }
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
